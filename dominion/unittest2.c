2131e24611d503f97330a111e9a24a84b57f5907
hardergeorge <harderg@oregonstate.edu>
2016-04-27 18:20:43 -0700

    lay the groundwork for assignment 2

--- dominion/unittest2.c
+++ dominion/unittest2.c
@@ -1,9 +1,11 @@
-#include "dominion.h"
 #include <stdio.h>
+
+#include "dominion.h"
+
 
 int failed = 0;
 
-int myassert(int b,char* msg) {
+void myassert(int b,char* msg) {
   if (b == 0) {
     printf("FAILED ASSERTION: %s\n",msg);
     failed = 1;
@@ -17,17 +19,16 @@ void checkasserts() {
 }
 
 int main() {
+
   struct gameState g;
 
-  int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
-	       outpost,baron,tribute};
+  int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador, outpost,baron,tribute};
 
   int r = initializeGame(2, k, 5, &g);
 
   myassert(r == 0, "No duplicates, 2 players, should succeed");
 
-  int k2[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,
-	       outpost,baron,adventurer};
+  int k2[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador, outpost,baron,adventurer};
 
   r = initializeGame(2, k2, 5, &g);
 
@@ -38,4 +39,6 @@ int main() {
   myassert(r == 0,"I should be allowed to play with a lot of people!");
 
   checkasserts();
+
+  return 0;
 }
